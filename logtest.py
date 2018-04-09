fb90070e18b6a249b6dd7410262d73c7cfcbe408
RajaSrinivasan <s@srin.me>
2018-04-08 21:43:55 -0400

    process all RLMs

--- logtest.py
+++ logtest.py
@@ -3,21 +3,28 @@
 import logtools
 import sys
 REMOVELOGS=True
-rlmid=sys.argv[1]
-today=logtools.Today()
-logdates = logtools.FindUniqueLogDates(rlmid)
-print(logdates)
-for logdate in logdates:
-    print( "Date : %s" % logdate)
-    if logdate == today:
-        print("Leaving out today's files")
-    else:
-        logtools.PackAllFiles(rlmid,logdate,REMOVELOGS)
+rootfolder=sys.argv[1]
 
-print( logtools.Yesterday())
-yl=logtools.YesterLogFiles(rlmid)
-print(yl)
-if len(yl) == 0:
-    print("%s had no logs yesterday" % rlmid)
-else:
-    print("%s had some logs yesterday (%d)" % (rlmid,len(yl)))
+def ProcessRLM(rlmid):
+    logdates = logtools.FindUniqueLogDates(rlmid)
+    print(logdates)
+    for logdate in logdates:
+        print( "Date : %s" % logdate)
+        if logdate == today:
+            print("Leaving out today's files")
+        else:
+            logtools.PackAllFiles(rlmid,logdate,REMOVELOGS)
+
+    print( logtools.Yesterday())
+    yl=logtools.YesterLogFiles(rlmid)
+    print(yl)
+    if len(yl) == 0:
+        print("%s had no logs yesterday" % rlmid)
+    else:
+        print("%s had some logs yesterday (%d)" % (rlmid,len(yl)))
+
+today=logtools.Today()
+rlmfolders=logtools.IdentifyRLMs(rootfolder)
+for rlmf in rlmfolders:
+    ProcessRLM(rlmf)
+
