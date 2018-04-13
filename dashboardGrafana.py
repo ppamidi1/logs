050fa657dcb091ac15298b01b1ef8a7f6a78f830
ppamidi1 <ppamidi@abiomed.com>
2018-04-13 16:32:42 -0400

    Initial Commit for pushing data to Graphite

---
+++ dashboardGrafana.py
@@ -0,0 +1,92 @@
+import sys
+import time
+import os
+import socket
+import telnetlib
+import json
+import extract
+
+
+class RenderLogs(object):
+    def __init__(self):
+        #self.console_ip = sys.argv[1]
+        #self.connect_telnet()
+        self.connect_graphite()
+
+    def connect_graphite(self):
+        """ Connect the graphite server
+
+        Arguments(s):
+        None
+        
+        Return(s):
+        None
+        """
+
+        CARBON_SERVER = '172.16.1.149'
+        CARBON_PORT = 2003
+        self.sock = socket.socket()
+        self.sock.connect((CARBON_SERVER, CARBON_PORT))
+
+    def getReboots(rc, vst):
+        reboots = rc/((vst+1)/(3600*24))
+        return reboots
+
+    def getUpTime(nmt, vst):
+        upTime = (1-(nmt-vst)/nmt)*100
+        return upTime
+    
+    def getFilesPerDay(frc, vst):
+        files = frc/(vst/(3600*24))
+        return files
+    
+    def send_data_to_graphite(self):
+        """
+        
+        Return(s):
+        None
+        """
+        #ip = self.console_ip.replace('.', '_')
+        extract.main()
+        d = os.listdir()
+        delay = 1
+        for files in d:
+            if ("RL" in files and ".txt" in files):
+                data = json.load(open(files))   
+                f = files[0:7]
+                rc = int(data[f][3].get('rc'))
+                vst = int(data[f][3].get('vst'))
+                nmt = int(data[f][3].get('nmt'))
+                frc = int(data[f][3].get('frc'))
+
+                reboots = rc/((vst+1)/(3600*24))
+                upTime = (1-(nmt-vst)/nmt)*100
+                #return(f,getReboots(rc,vst),getUpTime(nmt,vst),getFilesPerDay(frc,vst))
+                print ('RLname : %s Reboot : %d Uptime : %d' % (f,reboots,upTime))
+                timestamp = int(time.time())
+                lines = [
+                    'RLname.%s.UpTime %d %d' % (f, upTime, timestamp)
+                    ]
+                message = '\n'.join(lines) + '\n'
+                print(message)
+                self.sock.send(message.encode("utf-8"))
+                time.sleep(delay)
+                
+        #RLname = 'RL00004'
+        #timestamp = os.system ('ls /home/ppamidi/RL00004/journal/2*stats.log | tail -1 | grep -o "[0-9]\+stats" | sed "s/[^0-9]*//g" >')
+        
+        #UpTime = os.system ('cat "$(ls /home/ppamidi/RL00004/journal/2*stats.log | tail -1)" | grep "Up Time:" | grep -Eo "\b[0-9]+\b"')
+        
+        #print(timestamp)
+        
+if __name__ == '__main__':
+    performance = RenderLogs()
+    try:
+        while True:
+            #data = performance.get_data_from_console()
+            performance.send_data_to_graphite()
+            break;
+    except KeyboardInterrupt:
+        print("Key Pressed...")
+    finally:
+        print("Script exited!!!")
---
+++ extract.py
@@ -0,0 +1,71 @@
+import os
+import json
+import time
+
+def find():
+    d = os.listdir()
+    RLM = []
+    for dirs in d:
+        if(("RL0" in dirs) and ".txt" not in dirs):
+            RLM.append(dirs)
+    return(RLM)
+
+
+def getStatLogs(RLM):
+
+    for rl in RLM:
+        stats = []
+        logs = os.listdir('/home/ppamidi/RemoteLinkLogs/'+rl+'/journal')
+        if(logs != []):
+            for files in logs:
+                if ('stats.log' in files and '.zip' not in files):
+                    stats.append(files)
+                    stats.sort()
+            if(len(stats) != 1):
+                del stats[-1]
+                f = stats[-1]
+                statLogs = convert(rl,f)
+    return statLogs
+
+
+def convert(rl,f):
+    statDict = {}
+    statLogFile = open("/home/ppamidi/RemoteLinkLogs/"+rl+"/journal/"+f,"r")
+    stlog = statLogFile.read()
+    lines =stlog.splitlines()
+    if (len(lines) > 1):
+        SOM = {}
+        SOMData =  lines[0].split(":")
+        SOM[SOMData[0]]=SOMData[1]
+
+        Network = {}
+        NetData = lines[8].split(":")
+        Network[NetData[0]] = NetData[1]
+
+        UpTime = {}
+        UpData = lines[17].split(":")
+        UpTime[UpData[0]] = UpData[1]
+
+        keys = lines[23].split(",")
+        values = lines[24].split(",")
+        Chart = dict(zip(keys, values))
+
+        data = [SOM,Network,UpTime,Chart]
+        statDict[rl] = data
+
+    with open(rl + ".txt", "w") as json_data:
+        json.dump(statDict, json_data)
+    json_data.close()
+    return json_data
+
+
+def main():
+    RLM = find()
+    statLogs = getStatLogs(RLM)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
